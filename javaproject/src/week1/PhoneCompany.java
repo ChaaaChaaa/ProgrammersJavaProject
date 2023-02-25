e430195bc1bb06b90523f4bbe7a475c666a9b484
peony <peony@bluehole.net>
2023-02-25 18:50:13 +0900

    feat phone company to use Enum

---
+++ javaproject/src/week1/PhoneCompany.java
@@ -0,0 +1,15 @@
+package week1;
+
+public enum PhoneCompany {
+    SAMSONG("삼송폰"), APPLE("사과폰");
+
+    final String phoneCompanyName;
+
+    PhoneCompany(String phoneCompanyName) {
+        this.phoneCompanyName = phoneCompanyName;
+    }
+
+    public String getPhoneCompanyName() {
+        return phoneCompanyName;
+    }
+}
