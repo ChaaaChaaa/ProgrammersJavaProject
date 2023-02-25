34638ab9400cf47dc0c75d60c036ba36d7d5b94d
peony <peony@bluehole.net>
2023-02-25 18:52:52 +0900

    feat Main Controller

---
+++ javaproject/src/week1/Main.java
@@ -0,0 +1,21 @@
+package week1;
+
+public class Main {
+    public static void main(String[] args) {
+        PhoneShop phoneShop = new PhoneShop();
+
+        User user1 = new User("잡th");
+        Phone samsongPhone = new SamsongPhone(user1);
+        phoneShop.buyPhone(user1,samsongPhone);
+        samsongPhone.turnOnPhone();
+        samsongPhone.printPhoneState();
+
+        System.out.println();
+
+        User user2 = new User("거니");
+        Phone applePhone = new ApplePhone(user2);
+        phoneShop.buyPhone(user2,applePhone);
+        applePhone.turnOnPhone();
+        applePhone.printPhoneState();
+    }
+}
