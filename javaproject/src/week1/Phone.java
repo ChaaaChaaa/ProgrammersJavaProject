527d52e329037736c153d72a190dcbda7dbc95c7
peony <peony@bluehole.net>
2023-02-25 18:50:49 +0900

    feat phone for multiple phone company

---
+++ javaproject/src/week1/Phone.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Phone implements PhoneInterface{
+   PhoneCompany phoneCompany;
+    public PhoneCompany getPhoneCompany() {
+        return phoneCompany;
+    }
+
+    @Override
+    public void turnOnPhone() {
+
+    }
+
+    @Override
+    public void printPhoneState() {
+
+    }
+}
